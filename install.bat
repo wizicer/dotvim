00e23cab7e4a06a5d5394a2371397b0e6e3543c7
Icer <liangshuangde@163.com>
2016-05-11 08:55:58 +0800

    update install script for .vim folder

--- install.bat
+++ install.bat
@@ -1,30 +1,13 @@
 @echo off
-rem as put vim file to $HOME directory, no need administrator privilege
-rem goto check_Permissions
-goto link_file
-
-:check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Success: Administrative permissions confirmed.
-    ) else (
-        echo Failure: Current permissions inadequate.
-        pause
-        goto end
-    )
-    cd /d "%~dp0"
 
 :link_file
     cd ..
-    rem for /f %%i in ('dir vimfiles\vimrc* /b') do mklink _%%i vimfiles\%%i /h
     if exist _vimrc del _vimrc
-    mklink _vimrc vimfiles\vimrc /h
+    mklink _vimrc .vim\vimrc /h
     if exist _vsvimrc del _vsvimrc
-    mklink _vsvimrc vimfiles\_vsvimrc /h
+    mklink _vsvimrc .vim\_vsvimrc /h
 
 :update_submodule
-    cd vimfiles
+    cd .vim
     git submodule update --init --recursive
 :end
